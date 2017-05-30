1580e0c97d2b3fbb28bf2b38fc581c6254d339a3
youjin <youjinp6801@gmail.com>
2017-05-31 01:19:33 +0900

    통합 테스트

---
+++ spring-practice/src/test/java/kr/or/connect/persistence/BookDaoTest.java
@@ -0,0 +1,43 @@
+package kr.or.connect.persistence;
+
+import kr.or.connect.AppConfig;
+import kr.or.connect.domain.Book;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//밑에 is(), assertThat()를 위해서...
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*; 
+
+@RunWith(SpringRunner.class) //테스트전에 spring의 ApplicationContext를 로딩
+@ContextConfiguration(classes = AppConfig.class) //ApplicationContext에 등록된 bean을 지정한 설정파일이나 클래스를 지정
+public class BookDaoTest {
+	@Autowired //AppConfig에 정의된 BookDao를 참조 
+	private BookDao dao;
+	
+	//BookDao의 countBooks() 테스트
+	@Test
+	public void shouldCount() {
+		int count = dao.countBooks();
+		System.out.println(count);
+	}
+	
+	//insert 후 생성된 id로 select되는지 확인
+	@Test
+	public void shouldInsertAndSelect() {
+		// given
+		Book book = new Book("Java 웹개발", "네이버", 342);
+
+		// when
+		Integer id = dao.insert(book);
+
+		// then
+		Book selected = dao.selectById(id);
+		System.out.println(selected);
+		assertThat(selected.getTitle(), is("Java 웹개발"));
+	}
+}

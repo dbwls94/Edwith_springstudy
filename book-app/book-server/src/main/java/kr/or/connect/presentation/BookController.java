1ffd36ef03c7ea3326082d3670ecb0d9c54d49ba
youjin <youjinp6801@gmail.com>
2017-05-31 16:55:42 +0900

    Post API & MockMvc Test

--- book-app/book-server/src/main/java/kr/or/connect/presentation/BookController.java
+++ book-app/book-server/src/main/java/kr/or/connect/presentation/BookController.java
@@ -8,8 +8,12 @@ import kr.or.connect.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/api/books") //공통적인 부분
@@ -31,5 +35,11 @@ public class BookController {
 	@GetMapping("/{id}")
 	Book read(@PathVariable  Integer id) {
 		return service.findById(id);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	Book create(@RequestBody Book book) {
+		return service.create(book);
 	}
 }
--- book-app/book-server/src/main/java/kr/or/connect/service/BookService.java
+++ book-app/book-server/src/main/java/kr/or/connect/service/BookService.java
@@ -2,6 +2,9 @@ package kr.or.connect.service;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import kr.or.connect.domain.Book;
 
@@ -10,7 +13,10 @@ import org.springframework.stereotype.Service;
 //@Controller -> @Service -> @Repository
 @Service //Spring에서 component scan의 대상으로 인지됨
 public class BookService {
-	public Book findById(Integer id) {
+	private ConcurrentMap<Integer, Book> repo = new ConcurrentHashMap<>();
+	private AtomicInteger maxId = new AtomicInteger(0);
+	
+	/*public Book findById(Integer id) {
 		return new Book(1, "Java 이렇게 공부하자", "김자바", 300);
 	}
 
@@ -19,5 +25,21 @@ public class BookService {
 			new Book(1, "네이버 네비 좋아요", "김광근", 300),
 			new Book(2, "HTTP 완벽 이해하기", "김명호", 300)
 		);
+	}*/
+	
+	public Book findById(Integer id) {
+		return repo.get(id);
+	}
+
+	public Collection<Book> findAll() {
+		return repo.values();
+	}
+
+	//Book 객체 저장
+	public Book create(Book book) {
+		Integer id = maxId.addAndGet(1);
+		book.setId(id);
+		repo.put(id, book);
+		return book;
 	}
 }
